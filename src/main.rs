e4c52fdea71469ed9caf40ff9facba53e7c509f7
Discookie <matekos17@fazekas.hu>
2019-07-04 17:53:54 +0200

    Refactor roles into HashMap in main loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![recursion_limit = "128"]
 #![feature(drain_filter)]
 extern crate fern;
+extern crate hashbrown;
 #[macro_use]
 extern crate log;
 extern crate screeps;
@@ -12,6 +13,8 @@ mod military;
 mod roles;
 mod tasks;
 mod traits;
+
+use hashbrown::HashMap;
 
 use screeps::{
     constants::{
@@ -87,36 +90,43 @@ fn game_loop() {
     let _task_root = root().dict_or_create("tasks").unwrap();
     let role_root = root().dict_or_create("roles").unwrap();
 
+        
     let task_build = TaskBuild::new();
     let task_harvest = TaskHarvest::new();
     let task_refill = TaskRefill::new();
     let task_upgrade = TaskUpgrade::new();
 
-    // TODO clean this up and use HashMaps or some better resolver
-    let role_builder = Builder::new(role_root.dict_or_create("builder").unwrap(),
-                                   &task_build, &task_harvest, &task_refill, &task_upgrade);
-    let role_harvester = Harvester::new(role_root.dict_or_create("harvester").unwrap(),
+    let roles = {
+        let mut map: HashMap<&str, Box<dyn Role>> = HashMap::new();
+
+        // TODO clean this up and use HashMaps or some better resolver
+        let role_builder = Builder::new(role_root.dict_or_create("builder").unwrap(),
                                        &task_build, &task_harvest, &task_refill, &task_upgrade);
-    let role_upgrader = Upgrader::new(role_root.dict_or_create("upgrader").unwrap(),
-                                     &task_harvest, &task_upgrade);
+        let role_harvester = Harvester::new(role_root.dict_or_create("harvester").unwrap(),
+                                           &task_build, &task_harvest, &task_refill, &task_upgrade);
+        let role_upgrader = Upgrader::new(role_root.dict_or_create("upgrader").unwrap(),
+                                         &task_harvest, &task_upgrade);
+
+        map.insert(role_builder.name(), Box::from(role_builder));
+        map.insert(role_harvester.name(), Box::from(role_harvester));
+        map.insert(role_upgrader.name(), Box::from(role_upgrader));
+
+        map
+    };
+
     
 
     for creep in creeps::values() {
-        match &creep.memory().string("role").unwrap_or( Some("error".to_string()) ).unwrap_or( "missing".to_string() )
-        {
-            r if r == role_builder.name() => role_builder.run(&creep),
-            r if r == role_harvester.name() => role_harvester.run(&creep),
-            r if r == role_upgrader.name() => role_upgrader.run(&creep),
-            role => Err(Box::from(format!("unknown role {}", role)))
-        }.unwrap_or_else(|err| {
+        let role_str = creep.memory().string("role").unwrap_or( Some("error".to_string()) ).unwrap_or( "missing".to_string() );
+        if let Err(err) = roles.get(role_str.as_str()).ok_or(Box::from(format!("unknown role {}", role_str)))
+                            .and_then(|role| role.run(&creep)) {
             warn!("failed to execute task for creep {}: {}", creep.name(), err.to_string());
             err_counter += 1;
-        });
+        }
     }
 
     // New creep creation
     if let Some(spawn) = spawns::get("Spawn1") {
-        let mut spawning = spawn.is_spawning();
         fn next_id() -> i32 {
             let id = root().i32("id").unwrap_or(None).unwrap_or(0);
             id
@@ -141,41 +151,28 @@ fn game_loop() {
                 false
             }
         }
-        
-        if !spawning && role_harvester.run_count() < role_harvester.limit() {
-            let body = role_harvester.next_creep();
-            let id = next_id();
-            let options = SpawnOptions::new()
-                            .memory( make_mem(role_harvester.name(), id) );
 
-            spawning = spawn_creep(&spawn, &body, &id, &options);
-        } 
-        if !spawning && role_upgrader.run_count() < role_upgrader.limit() {
-            let body = role_upgrader.next_creep();
-            let id = next_id();
-            let options = SpawnOptions::new()
-                            .memory( make_mem(role_upgrader.name(), id) );
+        if !spawn.is_spawning() {
+            for (role_str, role) in roles.iter() {
+                let body = role.next_creep();
+                let id = next_id();
+                let options = SpawnOptions::new()
+                                .memory( make_mem(role.name(), id) );
 
-            spawning = spawn_creep(&spawn, &body, &id, &options);
-        }
-        if !spawning && role_builder.run_count() < role_builder.limit() {
-            let body = role_builder.next_creep();
-            let id = next_id();
-            let options = SpawnOptions::new()
-                            .memory( make_mem(role_builder.name(), id) );
-
-            spawning = spawn_creep(&spawn, &body, &id, &options);
+                if spawn_creep(&spawn, &body, &id, &options) {
+                    break;
+                }
+            }
         }
     }
 
     if time() % 50 == 3 {
         clear_deceased();
         
-        info!("harvesters: {}/{}, upgraders: {}/{}, builders: {}/{}", 
-            role_harvester.run_count(), role_harvester.limit(),
-            role_upgrader.run_count(), role_upgrader.limit(),
-            role_builder.run_count(), role_builder.limit()
-        );
+        info!("creep counts: <ul>{}</ul>", roles.iter().fold(String::new(), |acc, (role_str, role)| {
+                format!("{acc}<li>{name}: {run}/{limit}</li>", acc=acc, name=role_str, run=role.run_count(), limit=role.limit())
+            }
+        ));
     }
 
     if err_counter > 10 {
